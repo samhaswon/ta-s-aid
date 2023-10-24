b1a0f270ff12547e92f62ea4a96019fba3d83a5b
Samuel Howard <sahoward42@tntech.edu>
2023-10-23 23:01:54 -0500

    Added git test

---
+++ tests/test_git.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from modules.git import Git
+
+
+class TestGit(TestCase):
+    def test_clone(self):
+        with open("ids.txt", "r") as student_file:
+            student_list = student_file.read().splitlines()
+        repo_list = [(student, "lab-05-unittest") for student in student_list]
+        git = Git("gitlab.csc.tntech.edu:csc2310-sp23-students", True)
+        git.clone(repo_list, "test_destination")
