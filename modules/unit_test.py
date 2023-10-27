b1efed6645c74ba039c9651b81305628f884c58a
Samuel Howard <sahoward42@tntech.edu>
2023-10-27 13:26:50 -0500

    Added comments and docstrings

--- modules/unit_test.py
+++ modules/unit_test.py
@@ -6,19 +6,31 @@ import shutil
 
 
 class UnitTest(object):
-    def __init__(self, student_list: List[str], repo_directory: str, test_dir_name="test"):
+    def __init__(self, student_list: List[str], repo_directory: str, test_dir_name="test") -> None:
+        """
+        Create a UnitTest object to perform unit-testing of student code
+        :param student_list: List of student IDs that are used as the name of folders for their repos
+        :param repo_directory: directory that the repos are in
+        :param test_dir_name: (optional) Name of the test directory in the repo, default of "test"
+        """
         self.__repo_directory = repo_directory
         self.__student_list = student_list
         self.__test_dir = test_dir_name
 
+        # Figure out what this system's python command is
         if not os.system("python3 --version"):
             self.__py_cmd = "python3"
         elif not os.system("python --version"):
             self.__py_cmd = "python"
         else:
             self.__py_cmd = "py"
+        print(f"Using python command: {self.__py_cmd}")
 
-    def do_tests(self):
+    def do_tests(self) -> None:
+        """
+        Run all student tests in a given directory
+        :return: None
+        """
         results = []
         for student in self.__student_list:
             print(f"Running tests for {student}")
@@ -27,10 +39,13 @@ class UnitTest(object):
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()[1].decode("utf-8")
             results.append((student, output))
         for student, output in results:
+            # Get the run summary, counting the errors, failures, and passes
             run_result = output.splitlines()[0]
             error_count = run_result.count("E")
             failure_count = run_result.count("F")
             pass_count = run_result.count(".")
+
+            # Add a summary to the output file
             summary = good_job() \
                 if not error_count \
                 and not failure_count \
@@ -40,5 +55,7 @@ class UnitTest(object):
                      f"Passed: {pass_count}\n" + \
                      f"Total: {error_count + failure_count + pass_count}\n" + \
                      "=" * shutil.get_terminal_size((80, 20)).columns + "\n\n"
+
+            # Save the run result
             with open(f"{self.__repo_directory}/{student}.txt", "w") as output_file:
                 output_file.write(summary + output)
