c14fcfbd1ee65d14eb750ecffe1dab46d6b23c18
Samuel Howard <sahoward42@tntech.edu>
2023-10-27 13:19:34 -0500

    Added good_job.py for congratulation message

---
+++ modules/good_job.py
@@ -0,0 +1,31 @@
+import random
+
+
+def good_job() -> str:
+    """
+    Random congratulation message
+    :return: (string) congratulation message
+    """
+    congratulations = [
+        "Good job!",
+        "Well done!",
+        "Excellent work!",
+        "Fantastic!",
+        "You did great!",
+        "Keep up the good work!",
+        "You're doing a great job!",
+        "That's the way to go!",
+        "Keep it up!",
+        "Nice work!",
+        "You nailed it!",
+        "Brilliant job!",
+        "That's some top-notch work!",
+        "You've outdone yourself!",
+        "Code executed successfully!",
+        "Your code works perfectly!",
+        "Syntax Error Free!",
+        "Exceptionally bug-free code!",
+        "Your code is running smoothly!"
+        "Your code is error-free!"
+    ]
+    return random.choice(congratulations)
