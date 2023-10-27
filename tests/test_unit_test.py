531808804f599f965cc225265d91b31a4f6dc5fe
Samuel Howard <sahoward42@tntech.edu>
2023-10-27 13:51:59 -0500

    Improved tests

--- tests/test_unit_test.py
+++ tests/test_unit_test.py
@@ -4,7 +4,10 @@ from modules.unit_test import UnitTest
 
 class TestUnitTest(TestCase):
     def test_do_tests(self):
-        with open("ids.txt", "r") as student_file:
-            student_list = student_file.read().splitlines()
-        unit_test = UnitTest(student_list, "test_destination")
-        unit_test.do_tests()
+        try:
+            with open("ids.txt", "r") as student_file:
+                student_list = student_file.read().splitlines()
+            unit_test = UnitTest(student_list, "test_destination")
+            unit_test.do_tests()
+        except Exception as e:
+            self.fail(f"Failure: \n\n{e}")
