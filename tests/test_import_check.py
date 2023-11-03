99d9e65822116d070b3cf447143915e10c32a728
Samuel Howard <sahoward42@tntech.edu>
2023-11-03 13:31:21 -0500

    Added sanity check

--- tests/test_import_check.py
+++ tests/test_import_check.py
@@ -5,10 +5,13 @@ import os
 
 class TestImportCheck(TestCase):
     def test_check(self):
+        # Make some files for each "student" folder with some lines
         folders = [x[1] for x in os.walk("imports")][0]
         for folder in folders:
             with open(f"imports{os.path.sep + folder}.txt", "w") as test_file:
                 test_file.write("Some output\n")
+
+        # Do the import check
         import_check = ImportCheck("imports")
         import_check.check("file.py", "random")
 
@@ -33,3 +36,8 @@ class TestImportCheck(TestCase):
         with open(f"imports{os.path.sep}Jeff E.txt", "r") as jeff_file:
             self.assertIn("You did not import (and therefore did not use) random",
                           jeff_file.read())
+
+        # Sanity check each output file after the import check
+        for folder in folders:
+            with open(f"imports{os.path.sep + folder}.txt") as jeff_file:
+                self.assertIn("Some output\n", jeff_file.read())
