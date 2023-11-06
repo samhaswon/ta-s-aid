b02c8507cabf5dc9fd99a613d094919a87313721
Samuel Howard <sahoward42@tntech.edu>
2023-11-06 16:12:12 -0600

    Account for refactor + more rigorous tests

--- tests/test_unit_test.py
+++ tests/test_unit_test.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from modules.unit_test import UnitTest
+import os
 
 
 class TestUnitTest(TestCase):
@@ -7,7 +8,8 @@ class TestUnitTest(TestCase):
         try:
             with open("ids.txt", "r") as student_file:
                 student_list = student_file.read().splitlines()
-            unit_test = UnitTest(student_list, "test_destination")
+            unit_test = UnitTest("test_destination")
             unit_test.do_tests()
+            self.assertEqual(len(student_list), len([x[2] for x in os.walk("test_destination")][0]))
         except Exception as e:
             self.fail(f"Failure: \n\n{e}")
