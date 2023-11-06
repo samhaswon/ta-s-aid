38c54cecd9464c22b4caff0e021b25147bce52df
Samuel Howard <sahoward42@tntech.edu>
2023-11-06 11:46:55 -0600

    Cleaned up the typing a bit

--- modules/helpers/threadwrv.py
+++ modules/helpers/threadwrv.py
@@ -1,8 +1,9 @@
 from threading import Thread
+from typing import Any
 
 
 class ThreadWRV(Thread):
-    def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, Verbose=None):
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs={}):
         Thread.__init__(self, group, target, name, args, kwargs)
         self._return = None
 
@@ -10,6 +11,6 @@ class ThreadWRV(Thread):
         if self._target:
             self._return = self._target(*self._args, **self._kwargs)
 
-    def join(self, *args):
+    def join(self, *args) -> Any:
         Thread.join(self, *args)
         return self._return
