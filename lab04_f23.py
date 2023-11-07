3a437de931b523e7af6e5104960097b2feb0e937
Samuel Howard <sahoward42@tntech.edu>
2023-11-07 16:26:43 -0600

    Added accounting for multiple zip files

--- lab04_f23.py
+++ lab04_f23.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         exit(1)
 
     # Find the submission .zip file and extract it
-    submission_zip: str = [x for x in os.listdir(os.getcwd()) if x.endswith(".zip")][0]
+    submission_zip: str = [x for x in os.listdir(os.getcwd()) if x.endswith(".zip") and "Lab 04" in x][0]
     i_learn_zip = ILearnZip(submission_zip, zip_expected=False)
     i_learn_zip.extract()
     i_learn_zip.inject("./test_potion.py", "test_potion.py")
