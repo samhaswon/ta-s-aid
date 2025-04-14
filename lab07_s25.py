3b6c120cd74eb27683838b8fa1d42c524efac261
Samuel Howard <sahoward42@tntech.edu>
2025-04-14 09:29:51 -0500

    Lab 07 S25 inject test

--- lab07_s25.py
+++ lab07_s25.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 from modules.run import Run
+from modules.ilearnzip import ILearnZip
 import os
 
 if __name__ == '__main__':
     if not os.path.isfile("./lab05_s25_test.py"):
         print("Please place lab05_s25_test.py in this directory")
         exit(1)
+
+    i_learn_zip = ILearnZip("", zip_expected=True)
+    i_learn_zip.inject("./lab07_s25_test.py", "lab07_s25_test.py")
 
     # Run all the tests
     runner = Run("./submissions")
