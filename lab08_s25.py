a3ff02ccf62eeafd7d7469dab4cdcf92780800d3
Samuel Howard <sahoward42@tntech.edu>
2025-04-19 13:49:37 -0500

    Lab 08 S25 Init

---
+++ lab08_s25.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from modules.run import Run
+from modules.ilearnzip import ILearnZip
+from modules.plagiarism import Plagiarism
+import os
+
+if __name__ == '__main__':
+    if not os.path.isfile("./test_student_manager.py"):
+        print("Please place test_student_manager.py in this directory")
+        exit(1)
+    if not os.path.isfile("./test_student.py"):
+        print("Please place test_student.py in this directory")
+        exit(1)
+
+    # Check for plagiarism
+    plagiarism_check = Plagiarism(
+        "./submissions",
+        [
+            "test_potion.py",
+            ".DS_Store",
+            "README.md",
+            "README.pdf",
+            "ItemsClass.py",
+            "Items.py"
+        ],
+        [
+            r".*.csv",
+            r"images/.*",
+            r"__pycache__.*",
+            r"Data.*",
+            r"\.git.*",
+            r"\.idea.*"
+        ]
+    )
+    print(plagiarism_check.check_hash_str())
+
+    i_learn_zip = ILearnZip("", zip_expected=True)
+    i_learn_zip.inject("./test_student_manager.py", "tests/test_student_manager.py")
+    i_learn_zip.inject("./test_student.py", "tests/test_student.py")
+
+    # Run the tests
+    runner = Run("./submissions")
+    runner.run("echo \"Student tests\"; python3 -m unittest discover tests; "
+               "echo \"Our tests\"; python3 -m unittest discover .")
